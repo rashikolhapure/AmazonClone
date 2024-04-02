b1353ab081487de76bb59d7ca1bf92af2e569e24
rashikolhapure <rashikolhapure@gmail.com>
2024-04-02 16:30:50 +0530

    Add delete functionality to delete button in cart.js

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,6 +1,7 @@
-import { cart } from "../data/cart.js";
+import { cart, removeFromCart } from "../data/cart.js";
 import {products} from "../data/products.js";
 import { formatCurrency } from "./utils/money.js";
+
 
 let cartSummaryHTML = '';
 
@@ -37,7 +38,7 @@ cart.forEach((cartItem) => {
                 <span class="update-quantity-link link-primary">
                 Update
                 </span>
-                <span class="delete-quantity-link link-primary">
+                <span class="delete-quantity-link link-primary js-delete-link" data-product-id="${matchingProduct.id}">
                 Delete
                 </span>
             </div>
@@ -94,3 +95,12 @@ cart.forEach((cartItem) => {
 
 document.querySelector('.js-order-summary')
     .innerHTML = cartSummaryHTML;
+
+document.querySelectorAll('.js-delete-link')
+    .forEach((link) => {
+        link.addEventListener('click', () => {
+        const productId = link.dataset.productId;
+        removeFromCart(productId);
+        console.log (cart);
+        })
+    })
