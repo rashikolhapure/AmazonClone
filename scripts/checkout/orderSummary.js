5ad9378d3e6f0dca17a146b192b72049d0a509e8
rashikolhapure <rashikolhapure@gmail.com>
2024-04-03 17:11:51 +0530

    Save data and generate HTML for payment summary

--- scripts/checkout/orderSummary.js
+++ scripts/checkout/orderSummary.js
@@ -1,8 +1,8 @@
 import { cart, removeFromCart,updateDeliveryOptions } from "../../data/cart.js";
-import { products } from "../../data/products.js";
+import { getProduct, products } from "../../data/products.js";
 import { formatCurrency } from "../utils/money.js";
 import dayjs from "https://unpkg.com/dayjs@1.11.10/esm/index.js";
-import { deliveryOptions } from "../../data/deliveryOptions.js";
+import { deliveryOptions, getDeliveryOption } from "../../data/deliveryOptions.js";
 
 export function renderOrderSummary() {
 
@@ -10,15 +10,10 @@ let cartSummaryHTML = "";
 
 cart.forEach((cartItem) => {
   const productId = cartItem.productId;
-  let matchingProduct;
-  products.forEach((product) => {
-    if (product.id === productId) {
-      matchingProduct = product;
-    }
-  });
+  const matchingProduct = getProduct(productId);
 
   const deliveryOptionId = cartItem.deliveryOptionId;
-  let deliveryOption;
+  let deliveryOption = getDeliveryOption(deliveryOptionId)
   
   deliveryOptions.forEach((option) => {
     if(option.id === deliveryOptionId) {
