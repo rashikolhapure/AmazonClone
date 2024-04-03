3434bc5e83d21a4d9a4683e534e4234fd62f3526
rashikolhapure <rashikolhapure@gmail.com>
2024-04-03 15:58:55 +0530

    Make delivery options interactive

--- data/cart.js
+++ data/cart.js
@@ -53,3 +53,15 @@ export function removeFromCart(productId) {
   cart = newCart;
   saveToStorage();
 }
+
+export function updateDeliveryOptions(productId, deliveryOptionId) {
+  let matchingItem;
+
+  cart.forEach((cartItem) => {
+    if (productId === cartItem.productId) {
+      matchingItem = cartItem;
+    }
+  });
+  matchingItem.deliveryOptionId = deliveryOptionId;
+  saveToStorage();
+}
