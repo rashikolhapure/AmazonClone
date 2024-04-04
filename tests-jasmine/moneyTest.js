149813f25d4529254b0d5551188e10b8e1e516d6
rashikolhapure <rashikolhapure@gmail.com>
2024-04-04 14:50:17 +0530

    Remove defaulr tests and add money tests

---
+++ tests-jasmine/moneyTest.js
@@ -0,0 +1,15 @@
+import {formatCurrency} from '../scripts/utils/money.js';
+
+describe('test suite: format currency', () => {
+    it('Converts cents into dollars', () => {
+        expect(formatCurrency(2095)).toEqual('20.95');
+    });
+
+    it('Works with zero', () => {
+        expect(formatCurrency(0)).toEqual('0.00');
+    });
+
+    it('Rounds upto nearest cents', () => {
+        expect(formatCurrency(2000.5)).toEqual('20.01');
+    });
+});
